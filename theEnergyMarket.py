7a625e11fd591cd218f32a6dc4ba48bad3f42845
BarisSayil <45429224+BarisSayil@users.noreply.github.com>
2018-12-27 19:24:08 +0100

    Update theEnergyMarket.py

--- theEnergyMarket.py
+++ theEnergyMarket.py
@@ -1,40 +1,65 @@
 from multiprocessing import Process, Value
 import sysv_ipc
 import random
-import time
-#test git
+from time import sleep
+
 class Home(Process):
+
+	numberOfHomes=0
 
 	def __init__(self,productionRate, consumptionRate):
 		super().__init__()
-		numberOfHomes.value+=1
-		self.budget=100
+		Home.numberOfHomes+=1
+		self.budget=1000
 		self.productionRate=productionRate
 		self.consumptionRate=consumptionRate
 		self.energy=0
-		self.homeNumber=numberOfHomes.value
-		self.mq = sysv_ipc.MessageQueue(self.homeNumber, sysv_ipc.IPC_CREAT)
+		self.homeNumber=Home.numberOfHomes
+		self.mq = sysv_ipc.MessageQueue(127)
 
 	def run(self):
 		while 1:
-			print("I'm home number {} and my budget is {} dollars.".format(self.homeNumber,self.budget))
-			time.sleep(random.uniform(1,2))
+			print("Home {}: My budget is {} dollars.".format(self.homeNumber,self.budget))
 			self.energy=self.productionRate-self.consumptionRate
+
 			if self.energy<0:
 				self.buy()
-			#if self.energy>0:
-			#	self.sell()
+			elif self.energy>0:
+				self.sell()
+
+
 			if self.budget<0:
-				print("Shit I'm broke!")
-				self.mq.send(str(0).encode())
+				print("Home {}: Shit I'm broke!".format(self.homeNumber))
+				self.sendMessageQueue(-1)		#Send message queue to Market to let it know that a home has gone bankrupt.
 				break
 			else:
-				self.mq.send(str(1).encode())
+				self.sendMessageQueue(1)
+
+
+			sleep(5)
+
+	def sendMessageQueue(self,n):
+		self.mq.send(str(n).encode())
+
+	def receiveMessageQueue(self):
+		message, t = self.mq.receive()
+		value = message.decode()
+		value = int(value)
+		return value
 
 
 	def buy(self):
-		self.budget+=self.energy*price.value
-		price.value+=.1							#The price goes up everytime someone buys energy.
+		print("Home {}: What's the price?".format(self.homeNumber))
+		self.sendMessageQueue('Buy')					#Ask the price
+		price=self.receiveMessageQueue()
+		print("Home {}: It seems the price is {} dollars.".format(self.homeNumber,price))
+		self.budget+=self.energy*price
+		# market.price+=.1							#The price goes up everytime someone buys energy.
+
+
+	# def sell(self):
+	# 	self.budget+=self.energy*market.price
+	# 	market.price-=.1							#The price goes down everytime someone sells energy.
 
 
 
@@ -42,46 +67,52 @@ class Market(Process):
 
 	def __init__(self):
 		super().__init__()
+		self.price=20
+		self.mq = sysv_ipc.MessageQueue(127,sysv_ipc.IPC_CREAT)
 
 	def run(self):
 		while 1:
 
-			if numberOfHomes.value==1:
-				mq = sysv_ipc.MessageQueue(numberOfHomes.value)
+			value=self.receiveMessageQueue()
 
 
-			message, t = mq.receive()
-			value = message.decode()
-			value = int(value)
-
-			print(value)
-			if value == 2:
-
-			if value==0:
-				print('No more homes alive :(')
+			if value==-1:
+				print('Market: No more homes alive :(')
 				break
 
+			if value=='Buy':
+				print('Market: The price of energy is %s dollars.' %self.price)
+				self.sendMessageQueue(self.price,1)
+				print('Market: Energy is bought.')
+				print("Market: Increasing the price")
+				self.price+=5
 
 
-			time.sleep(random.uniform(1,2))
-			price.value-=.2							#The price goes down over time if noone buys any energy.
-			print('The price of energy is %s dollars.' %price.value)
+			self.price-=1							#The price goes down over time if noone buys any energy.
+			print('Market: The price of energy is now %s dollars.' %self.price)
 
+			sleep(random.uniform(1,2))
 
+	def sendMessageQueue(self, n, h):
+		self.mq.send(str(n).encode())
 
-
-
+	def receiveMessageQueue(self):
+		message, t = self.mq.receive()
+		value = message.decode()
+		print(value)
+		return value
 
 
 def EnergyMarket():
 
 	market=Market()
-	home1=Home(0,10)
-
 	market.start()
+
+	sleep(1)
+
+	home1=Home(0,10)
 	home1.start()
 
 
-price=Value('d',2.0)
-numberOfHomes=Value('i',0)
-EnergyMarket()
+if __name__=="__main__":
+	EnergyMarket()
