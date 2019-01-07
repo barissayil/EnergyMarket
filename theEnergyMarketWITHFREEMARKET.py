f8c08c2fe8d328bfbb69004d5abb91aa12fe3c20
Ratatinator97 <asidiras.csi@gmail.com>
2019-01-07 17:49:29 +0100

    Working maybe

--- theEnergyMarketWITHFREEMARKET.py
+++ theEnergyMarketWITHFREEMARKET.py
@@ -8,18 +8,14 @@ import sys
 from threading import Thread, Semaphore
 
 
-
-
-
-
-
 class Home(Process):
 
 	numberOfHomes=0
 
-	def __init__(self, productionRate, consumptionRate):
+	def __init__(self, productionRate, consumptionRate, Generous):
 		sleep(1)
 		super().__init__()
+		self.generous= Generous
 		Home.numberOfHomes+=1
 		self.budget=1000
 		self.productionRate=productionRate
@@ -28,9 +24,10 @@ class Home(Process):
 		self.homeNumber=Home.numberOfHomes
 		self.name="Home{}".format(self.homeNumber)
 		self.messageQueue=MessageQueue(self.homeNumber,IPC_CREAT)
-
+		self.marketMessageQueue=MessageQueue(100)
+		self.freeMessageQueue=MessageQueue(200)
 		print("{}'s messageQueue: {}".format(self.name, self.messageQueue))
-		
+
 
 	def run(self):
 
@@ -44,59 +41,66 @@ class Home(Process):
 				self.sendMessage('Broke')
 				break
 
-			
-
-	def 
-
-
 
 
 	def decideWhatToDo(self):
 		print("Home {}: My budget is {} dollars.".format(self.homeNumber,self.budget))
 		self.energy=self.productionRate-self.consumptionRate
 		if self.energy<0:
-			self.buy()
+			self.buy("Free")
+			self.buy("Pay")
 		elif self.energy>0:
-			self.sell()
+			self.sell("Free")
+			self.sell("Pay")
 
 	def sendMessage(self, message, market):
 
-		if market=='normal':
+		if market=='Pay':
 			message= str(self.homeNumber) + " " + message
-			MessageQueue(100).send(str(message).encode())
+			self.marketMessageQueue.send(str(message).encode())
 			print("Home{} sent: {}".format(self.homeNumber,message))
 
-		else:
-			message= str(self.homeNumber) + " " + message
-			MessageQueue(200).send(str(message).encode())
+		elif market == "Free":
+			self.freeMessageQueue.send(str(message).encode())
 			print("Home{} sent: {}".format(self.homeNumber,message))
 
+	def receiveMessage(self,Market):
+		if Market == "Pay":
+			x, t = self.messageQueue.receive()
+			message = x.decode()
+			print("Home{} recieved: {} price".format(self.homeNumber, message))
+			message = int(message)
 
-		
-
-
-	def receiveMessage(self):
-		x, t = self.messageQueue.receive()
-		message = x.decode()
-		print("Home{} recieved: {}".format(self.homeNumber, message))
-		message = int(message)
+		elif Market == "Free":
+			x, t = self.freeMessageQueue.receive()
+			message = x.decode()
+			print("Home{} recieved: {} of free energy".format(self.homeNumber, message))
+			message = int(message)
 		return message
 
+	def buy(self,Method):
+		if Method == "Pay":
+			print("Home {}: What's the price? I wanna buy some energy.".format(self.homeNumber))
+			self.sendMessage('Buy',Method)
+			price=self.receiveMessage(Method)
+			print("Home {}: It seems the price is {} dollars.".format(self.homeNumber,price))
+			self.budget+=self.energy*price
 
-	def buy(self):
-		print("Home {}: What's the price? I wanna buy some energy.".format(self.homeNumber))
-		self.sendMessage('Buy')
-		price=self.receiveMessage()
-		print("Home {}: It seems the price is {} dollars.".format(self.homeNumber,price))
-		self.budget+=self.energy*price
+		elif Method == "Free":
+			amount= self.receiveMessage(Method)
+			self.energy= self.energy - int(amount)
 
+	def sell(self, Method):
+		if Method == "Pay":
+			print("Home {}: What's the price? I wanna sell some energy.".format(self.homeNumber))
+			self.sendMessage('Sell',Method)
+			price=self.receiveMessage(Method)
+			print("Home {}: It seems the price is {} dollars.".format(self.homeNumber,price))
+			self.budget+=self.energy*price
+		elif Method == "Free":
+			print("I gave free energy "+str(self.energy)+" units")
+			self.sendMessage(self.energy,"Free")
 
-	def sell(self):
-		print("Home {}: What's the price? I wanna sell some energy.".format(self.homeNumber))
-		self.sendMessage('Sell')
-		price=self.receiveMessage()
-		print("Home {}: It seems the price is {} dollars.".format(self.homeNumber,price))
-		self.budget+=self.energy*price
 
 
 	# def handleMessage(self):
@@ -149,7 +153,7 @@ class Market(Process):
 		print("Market: Creating requestsThread")
 		self.lookAtRequests()
 		print('Market: The price of energy is now %s dollars.' %self.price)
-		
+
 
 	def sendMessage(self, homeNumber, message):
 		MessageQueue(homeNumber).send(str(message).encode())
@@ -168,10 +172,10 @@ def cleaning():			#To clean the message queues.
 	clear.remove()
 
 	clear=MessageQueue(1,IPC_CREAT)
-	clear.remove()							
+	clear.remove()
 
 	clear=MessageQueue(2,IPC_CREAT)
-	clear.remove()	
+	clear.remove()
 
 
 if __name__=="__main__":
@@ -194,9 +198,9 @@ if __name__=="__main__":
 
 
 
-	home1=Home(0,10)
+	home1=Home(0,10,True)
 	home1.start()
 
 
-	home2=Home(10,8)
+	home2=Home(10,8,True)
 	home2.start()
